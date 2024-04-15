9d7e29ca38b03dc333516e76d0c651e28f114b2a
Lyfraus <85877466+Lyfraus@users.noreply.github.com>
2024-04-15 19:17:16 -0300

    First Upload
    - Full index
    - Header of:
      Crear
      Ver
      Editar

---
+++ js/index.js
@@ -0,0 +1,64 @@
+
+
+
+
+function handleResize(){
+
+    console.log(document.documentElement.clientWidth);
+    if (document.documentElement.clientWidth > 1207) {
+
+        let elements = document.getElementsByClassName("opcionmain");
+        
+        for (let i=0 ; i<(elements.length) ; i++) {
+
+            elements[i].querySelector("p").style.textAlign = "center";
+
+        }
+        console.log("mid");
+
+    }
+    else {
+
+        let elements = document.getElementsByClassName("opcionmain");
+        
+        for (let i=0 ; i<(elements.length) ; i++) {
+
+            elements[i].querySelector("p").style.textAlign = "left";
+
+        }
+        console.log("left");
+
+        if (document.documentElement.clientWidth < 934) {
+
+            document.getElementById("opciones").style.flexDirection = "column";
+            console.log("column");
+
+            let elements = document.getElementsByClassName("opcioninicio");
+        
+            for (let i=0 ; i<(elements.length) ; i++) {
+
+                elements[i].style.width = "90%";
+
+            }
+
+        }
+        else {
+
+            document.getElementById("opciones").style.flexDirection = "row";
+            console.log("row");
+
+            let elements = document.getElementsByClassName("opcioninicio");
+        
+            for (let i=0 ; i<(elements.length) ; i++) {
+
+                elements[i].style.width = "";
+
+            }
+
+        }
+
+    }
+
+}
+
+window.addEventListener("resize",handleResize);
